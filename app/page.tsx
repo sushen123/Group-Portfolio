386237c71b26804dd612c395f25d22e32f57a2a2
ShikharThaQuri <shikharchand4321@gmail.com>
2026-02-08 22:46:57 +0545

    new change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Hello</p>
+      <p>Hello World !!!</p>
+      <p>Hiiii</p>
     </div>
   );
 }
